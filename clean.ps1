789a220d5efc4e43e3c36f997cace64ab900de70
Eliah Kagan <degeneracypressure@gmail.com>
2024-12-21 15:03:55 -0500

    Replace the batch file with a PowerShell script
    This works as desired, at the expense of possibly being hard to
    run for some users, depending on how they have configured settings
    around unsigned PowerShell scripts, and depending on how they
    obtain the files in this repository.

---
+++ clean.ps1
@@ -0,0 +1,9 @@
+# This script cleans the test files. It does not delete any build artifacts.
+Remove-Item symlink-file-w
+Remove-Item symlink-dir-w
+Remove-Item target-file-w
+Remove-Item target-dir-w
+Remove-Item symlink-file-a
+Remove-Item symlink-dir-a
+Remove-Item target-file-a
+Remove-Item target-dir-a
